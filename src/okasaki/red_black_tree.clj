39a99cf0c7aa73a0b455d784d9c0625ae1145782
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2012-01-03 15:17:21 +0100

    Added implementation of Red-Black-Trees

---
+++ src/okasaki/red_black_tree.clj
@@ -0,0 +1,38 @@
+(ns okasaki.red-black-tree
+    (:use datatype.core))
+
+(defdatatype
+    ::Color
+    Red
+    Black)
+
+(defdatatype
+    ::Red-Black-Tree
+    Empty
+    (Node color left root right))
+
+(defun member [x t]
+    [x Empty] false
+    [x [Node _ a y b]] (cond (< x y) (recur x a)
+                             (> x y) (recur x b)
+                             :else true))
+
+(defun balance [color left root right]
+    [Black [Node Red [Node Red a x b] y c] z d] (->Node Red (->Node Black a x b) y (->Node Black c z d))
+    [Black [Node Red a x [Node Red b y c]] z d] (->Node Red (->Node Black a x b) y (->Node Black c z d))
+    [Black a x [Node Red [Node Red b y c] z d]] (->Node Red (->Node Black a x b) y (->Node Black c z d))
+    [Black a x [Node Red b y [Node Red c z d]]] (->Node Red (->Node Black a x b) y (->Node Black c z d))
+    :else                                       (->Node color left root right))
+
+(defn insert [x s]
+    (let [ins (fn ins [s]
+                  (caseof [s]
+                          [Empty] (->Node Red Empty x Empty)
+                          [[Node color a y b]] (cond (< x y) (balance color (ins a) y b)
+                                                     (> x y) (balance color a y (ins b))
+                                                     :else   s)))
+          res (ins s)
+          left (:node-left res)
+          root (:node-root res)
+          right (:node-right res)]
+        (->Node Black left root right)))
---
+++ test/okasaki/red_black_tree_tests.clj
@@ -0,0 +1,47 @@
+(ns okasaki.red-black-tree-tests
+    (:use okasaki.red-black-tree
+          datatype.core
+          clojure.test))
+
+(deftest tree-without-nodes
+    (is (not (member 1 Empty))))
+
+(deftest one-node-tree
+    (is (member 1 (insert 1 Empty)))
+    (is (not (member 1 (insert 2 Empty))))
+    (is (not (member 2 (insert 1 Empty)))))
+
+(deftest two-nodes-tree
+    (is (member 1 (insert 1 (insert 2 Empty))))
+    (is (member 1 (insert 2 (insert 1 Empty))))
+    (is (not (member 3 (insert 1 (insert 2 Empty)))))
+    (is (not (member 0 (insert 1 (insert 2 Empty))))))    
+
+(deftest many-nodes-tree
+    (let [elems (range 100)
+          rbt (reduce #(insert %2 %1) Empty elems)]
+        (is (member 50 rbt))
+        (is (not (member 1000 rbt)))))
+
+(defun all-color-paths [tree]
+          [Empty] [[]]
+          [[Node c l x r]] (let [ps (into (all-color-paths l) (all-color-paths r))]
+                               (map #(conj % c) ps)))
+
+(deftest no-red-node-has-a-red-child
+    (is (->> (range 100)
+             (reduce #(insert %2 %1) Empty)
+             all-color-paths
+             (mapcat (partial partition 2 1))
+             (not-any? (fn [[c p]] (and (= :okasaki.red-black-tree/Red p)
+                                       (= :okasaki.red-black-tree/Red c)))))))
+
+(deftest same-number-of-black-nodes-on-every-path
+    (is (= 1 (->> (range 100)
+                  (reduce #(insert %2 %1) Empty)
+                  all-color-paths
+                  (map (partial filter #(= :okasaki.red-black-tree/Black %)))
+                  (map count)
+                  set
+                  count))))
+
