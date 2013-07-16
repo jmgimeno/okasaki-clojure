ab9b50539d59c87c4848fcffe917be5f356ae304
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2013-07-16 09:54:05 +0200

    Updated versions of dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject okasaki "0.0.8"
   :description "Examples and exercices from Okasaki's Purely Functional Data Structures"
-  :dependencies [[clojure "1.3.0"]
-                 [org.clojure/core.match "0.2.0-alpha9"]
-                 ;;[match "0.2.0-alpha10-SNAPSHOT"]
-                ]
-  :dev-dependencies [[swank-clojure "1.3.4"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.match "0.2.0-rc3"]])
